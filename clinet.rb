e6e9d85ceab23d9418cef2e33c6272952a6e09cf
Douglas Ciole <douglasciole@gmail.com>
2019-10-09 15:14:15 -0700

    Server and Client sending/reciveing messages at sametime

--- clinet.rb
+++ clinet.rb
@@ -1,14 +1,22 @@
 require 'socket'
 
-socket = TCPSocket.open("192.168.43.119", 3000)
+socket = TCPSocket.open("127.0.0.1", 3000)
 
 puts "Starting the Client..................."
-
 message = socket
-a = gets.chomp
-message.puts a
-puts message.gets
-puts message.gets
 
-puts "Closing the Client..................."
-socket.close                    # Close the socket
+Thread.new {
+   loop{
+      m = message.gets
+      puts "Client typed this message: " + m
+   }
+
+}
+
+loop{
+  puts "Type a message: "
+  a = gets
+  puts "You typed: " + a
+
+  message.puts a
+}
--- server.rb
+++ server.rb
@@ -1,5 +1,4 @@
 require 'socket'
-require 'thread'
 
 puts "Starting the Server..................."
 server = TCPServer.open(3000) # Server would listen on port 3000
@@ -11,14 +10,12 @@ Thread.new {
       puts "Client typed this message: " + m
    }
 
-}.join
-Thread.new {
-   loop{                         # Servers run forever
-      puts "Type a message: "
-      a = gets
-      puts "You typed: " + a
+}
 
-      client_connection.puts a
-   }
+loop{                         # Servers run forever
+   puts "Type a message: "
+   a = gets
+   puts "You typed: " + a
 
-}.join
+   client_connection.puts a
+}
