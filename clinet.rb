7f30eee85c8339288568cf87c4441c162e79d9a6
Douglas Ciole <douglasciole@gmail.com>
2019-10-09 14:55:24 -0700

    Updated server

--- clinet.rb
+++ clinet.rb
@@ -1,13 +1,14 @@
 require 'socket'
 
-socket = TCPSocket.open("localhost", 3000)
+socket = TCPSocket.open("192.168.43.119", 3000)
 
 puts "Starting the Client..................."
 
 message = socket
 a = gets.chomp
 message.puts a
-puts message.chomp
+puts message.gets
+puts message.gets
 
 puts "Closing the Client..................."
 socket.close                    # Close the socket
--- server.rb
+++ server.rb
@@ -1,15 +1,24 @@
 require 'socket'
+require 'thread'
 
 puts "Starting the Server..................."
 server = TCPServer.open(3000) # Server would listen on port 3000
-loop{                         # Servers run forever
-   client_connection = server.accept # Establish client connect connection
-   m = client_connection.gets
 
-  puts "Client typed this message: " + m
+client_connection = server.accept # Establish client connect connection
+Thread.new {
+   loop{                         # Servers run forever
+      m = client_connection.gets
+      puts "Client typed this message: " + m
+   }
 
-   client_connection.puts(Time.now) # Send the time to the client
-   client_connection.puts("Closing the connection with #{client_connection}")
-   client_connection.close      # Disconnect from the client
-   puts "Client disconnected <<<<<<< "
-}
+}.join
+Thread.new {
+   loop{                         # Servers run forever
+      puts "Type a message: "
+      a = gets
+      puts "You typed: " + a
+
+      client_connection.puts a
+   }
+
+}.join
